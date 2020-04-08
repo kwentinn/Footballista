02d76f912ca0ebc7fa5092f30bf816a779274245
kwentinn <kwentinn@gmail.com>
2020-04-08 18:33:46 +0200

    Location updated

--- Footballista.Players/Location.cs
+++ Footballista.Players/Location.cs
@@ -1,9 +1,24 @@
-﻿namespace Footballista.Players
+﻿using System.Globalization;
+
+namespace Footballista.Players
 {
 	public class Location
 	{
 		public string City { get; }
 		public string ZipCode { get; }
-		public Country Country { get; }
+		public RegionInfo Country { get; }
+
+
+		public Location(string city, string countryCode)
+		{
+			City = city;
+			Country = new RegionInfo(countryCode);
+		}
+		public Location(string city, string zipCode, string countryCode)
+		{
+			City = city;
+			ZipCode = zipCode;
+			Country = new RegionInfo(countryCode);
+		}
 	}
 }
