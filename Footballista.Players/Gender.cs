d259e9bc0e6f23bb02f734db62d0d7f8a60d6185
kwentinn <kwentinn@gmail.com>
2020-04-08 18:33:38 +0200

    added gender class

---
+++ Footballista.Players/Gender.cs
@@ -0,0 +1,15 @@
+﻿namespace Footballista.Players
+{
+	public class Gender
+	{
+		private bool _isFemale = false;
+
+		private Gender(bool isFemale)
+		{
+			_isFemale = isFemale;
+		}
+
+		public static Gender Male => new Gender(false);
+		public static Gender Female => new Gender(true);
+	}
+}
