fb4e5c1d3927e318cbefa10180aaf938248f9f32
kwentinn <kwentinn@gmail.com>
2020-04-09 18:40:10 +0200

    goalie physical features

---
+++ Footballista.Players/Features/Goalies/GoaliePhysicalFeature.cs
@@ -0,0 +1,8 @@
+﻿namespace Footballista.Players.Features.Goalies
+{
+	public class GoaliePhysicalFeature : PhysicalFeature
+	{
+		public GoaliePhysicalFeature(FeatureName name) : base(name) { }
+		public GoaliePhysicalFeature(FeatureName name, FeatureValue value) : base(name, value) { }
+	}
+}
---
+++ Footballista.Players/Features/Goalies/GoaliePhysicalFeatureSet.cs
@@ -0,0 +1,10 @@
+﻿namespace Footballista.Players.Features.Goalies
+{
+	public class GoaliePhysicalFeatureSet : PhysicalFeatureSet
+	{
+		public GoaliePhysicalFeatureSet()
+		{
+			_items.Add(new GoaliePhysicalFeature(FeatureName.Finishing));
+		}
+	}
+}
