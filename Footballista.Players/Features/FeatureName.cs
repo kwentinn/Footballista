09163883c22cca15f179561dab0e309dbdff8ec7
kwentinn <kwentinn@gmail.com>
2020-04-09 18:38:51 +0200

    feature name & value & physicalfeatureset

---
+++ Footballista.Players/Features/FeatureName.cs
@@ -0,0 +1,16 @@
+﻿using Footballista.BuildingBlocks.Domain;
+
+namespace Footballista.Players.Features
+{
+	public class FeatureName : ValueObject
+	{
+		public string Name { get; }
+		protected FeatureName(string name)
+		{
+			Name = name;
+		}
+
+		public static FeatureName RunningSpeed => new FeatureName(nameof(RunningSpeed));
+		public static FeatureName Finishing => new FeatureName(nameof(Finishing));
+	}
+}
---
+++ Footballista.Players/Features/PhysicalFeature.cs
@@ -0,0 +1,23 @@
+﻿namespace Footballista.Players.Features
+{
+	public class PhysicalFeature
+	{
+		public FeatureName Name { get; }
+		public FeatureValue Value { get; protected set; }
+
+		public PhysicalFeature(FeatureName name)
+		{
+			Name = name;
+		}
+		public PhysicalFeature(FeatureName name, FeatureValue value)
+		{
+			Name = name;
+			Value = value;
+		}
+
+		public void ChangeValue(FeatureValue value)
+		{
+			Value = value;
+		}
+	}
+}
