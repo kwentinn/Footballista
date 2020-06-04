4c1001116b807b8fdedc516415982ae373b3c2f6
kwentinn <kwentinn@gmail.com>
2020-06-04 18:56:36 +0200

    new object season

---
+++ Footballista.Wasm/Shared/Data/Season.cs
@@ -0,0 +1,17 @@
+﻿namespace Footballista.Wasm.Shared.Data
+{
+	public class Season
+	{
+		public int Start { get; }
+		public int End { get; }
+
+		public Season(int start, int end)
+		{
+			Start = start;
+			End = end;
+		}
+
+		public override string ToString() 
+			=> $"{Start}-{End}";
+	}
+}
