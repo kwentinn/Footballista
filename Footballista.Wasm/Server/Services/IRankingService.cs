64491a604b53579c0b94684b6ad6bd4b4df67c65
kwentinn <kwentinn@gmail.com>
2020-06-04 18:44:27 +0200

    ranking service mock

---
+++ Footballista.Wasm/Server/Services/IRankingService.cs
@@ -0,0 +1,102 @@
+﻿using Footballista.Wasm.Shared.Data.Competitions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Footballista.Wasm.Server.Services
+{
+	public interface IRankingService
+	{
+		Task<List<CompetitionRanking>> GetCompetitionRankingsAsync();
+	}
+	public class RankingService : IRankingService
+	{
+		public async Task<List<CompetitionRanking>> GetCompetitionRankingsAsync()
+		{
+			await Task.Delay(1000);
+
+			return new List<CompetitionRanking>
+			{
+				new CompetitionRanking
+				{
+					ClubName = "Montpellier",
+					Position = 1,
+					GamesPlayed = 38,
+					Won = 25,
+					Drawn = 7,
+					Lost = 6,
+					GoalsFor = 68,
+					GoalsAgainst = 34,
+					GoalsDifference = 34,
+					Points = 82
+				},
+				new CompetitionRanking
+				{
+					ClubName = "Paris Saint-Germain",
+					Position = 2,
+					GamesPlayed = 38,
+					Won = 23,
+					Drawn = 10,
+					Lost = 5,
+					GoalsFor = 75,
+					GoalsAgainst = 41,
+					GoalsDifference = 34,
+					Points = 79
+				},
+				new CompetitionRanking
+				{
+					ClubName = "Lille",
+					Position = 3,
+					GamesPlayed = 38,
+					Won = 21,
+					Drawn = 11,
+					Lost = 6,
+					GoalsFor = 72,
+					GoalsAgainst = 39,
+					GoalsDifference = 33,
+					Points = 74
+				},
+				new CompetitionRanking
+				{
+					ClubName = "Olympique Lyonnais",
+					Position = 4,
+					GamesPlayed = 38,
+					Won = 19,
+					Drawn = 7,
+					Lost = 12,
+					GoalsFor = 64,
+					GoalsAgainst = 51,
+					GoalsDifference = 13,
+					Points = 64
+				},
+				new CompetitionRanking
+				{
+					ClubName = "Girondins de Bordeaux",
+					Position = 5,
+					GamesPlayed = 38,
+					Won = 16,
+					Drawn = 13,
+					Lost = 9,
+					GoalsFor = 53,
+					GoalsAgainst = 74,
+					GoalsDifference = 12,
+					Points = 61
+				},
+				new CompetitionRanking
+				{
+					ClubName = "Stade Rennais",
+					Position = 6,
+					GamesPlayed = 38,
+					Won = 17,
+					Drawn = 9,
+					Lost = 12,
+					GoalsFor = 53,
+					GoalsAgainst = 44,
+					GoalsDifference = 9,
+					Points = 60
+				}
+			};
+		}
+	}
+}
