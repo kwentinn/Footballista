878e2aaa385f4a867cf09ce500838fb895401aa4
kwentinn <kwentinn@gmail.com>
2020-06-14 18:31:52 +0200

    date extensions update

--- Footballista.Wasm/Shared/Extensions/DateExtensions.cs
+++ Footballista.Wasm/Shared/Extensions/DateExtensions.cs
@@ -7,9 +7,38 @@ namespace Footballista.Wasm.Shared.Extensions
 {
 	public static class DateExtensions
 	{
+		/// <summary>
+		/// Add months to the current date
+		/// </summary>
+		/// <param name="date"></param>
+		/// <param name="monthsToAdd"></param>
+		/// <returns></returns>
 		public static Date AddMonths(this Date date, int monthsToAdd)
 			=> new Date(date.DateTime.AddMonths(monthsToAdd));
+
+		/// <summary>
+		/// Add days to the current date
+		/// </summary>
+		/// <param name="date"></param>
+		/// <param name="daysToAdd"></param>
+		/// <returns></returns>
 		public static Date AddDays(this Date date, int daysToAdd)
 			=> new Date(date.DateTime.AddDays(daysToAdd));
+
+		/// <summary>
+		/// Returns the first day of the current date's month
+		/// </summary>
+		/// <param name="date"></param>
+		/// <returns></returns>
+		public static Date FirstDayOfMonth(this Date date) 
+			=> new Date(date.Year, date.Month);
+
+		/// <summary>
+		/// Returns the last day of the current date's month
+		/// </summary>
+		/// <param name="date"></param>
+		/// <returns></returns>
+		public static Date LastDayOfMonth(this Date date) 
+			=> new Date(date.Year, date.Month + 1).AddDays(-1);
 	}
 }
