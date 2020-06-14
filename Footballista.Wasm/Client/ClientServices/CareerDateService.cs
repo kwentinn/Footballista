31e88018f2cbed93069cab24ca4985ea165d168d
Quentin Villevieille <quentin.villevieille@studi.fr>
2020-06-14 15:38:08 +0200

    display dates service & Date extensions

--- Footballista.Wasm/Client/ClientServices/CareerDateService.cs
+++ Footballista.Wasm/Client/ClientServices/CareerDateService.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using Footballista.Wasm.Shared.Extensions;
 
 namespace Footballista.Wasm.Client.ClientServices
 {
@@ -14,18 +15,28 @@ namespace Footballista.Wasm.Client.ClientServices
 
 		public DayOfWeek FirstDayOfWeek => CultureInfo.InvariantCulture.DateTimeFormat.FirstDayOfWeek;
 
-		public string GetDisplayMonth(Date date) => date.DateTime.ToString("MMMM", CultureInfo.InvariantCulture);
+		//public string GetDisplayDayOfWeek(DayOfWeek dow)
+		//{
 
-		public string GetDisplayYear(Date date) => date.DateTime.ToString("yyyy", CultureInfo.InvariantCulture);
+		//}
+		public string GetDisplayDay(Date date) => date.DateTime.ToString("dddd", CultureInfo);
+		public string GetDisplayMonth(Date date) => date.DateTime.ToString("MMMM", CultureInfo);
+		public string GetDisplayYear(Date date) => date.DateTime.ToString("yyyy", CultureInfo);
+		public string GetDisplayFullDate(Date date) => date.DateTime.ToString("D", CultureInfo);
+
 
 		public int GetNumberOfDays(Date date) => Calendar.GetDaysInMonth(date.Year, date.Month);
 
 		public List<Date> GetDates(Date date)
 		{
+			Date firstDayOfMonth = new Date(date.Year, date.Month, 1);
+			
+			Console.WriteLine(firstDayOfMonth);
+
 			List<Date> dates = new List<Date>();
-			for (int i = 0; i < GetNumberOfDays(date); i++)
+			for (int i = 0; i < GetNumberOfDays(firstDayOfMonth); i++)
 			{
-				dates.Add(new Date(date.DateTime.AddDays(i)));
+				dates.Add(firstDayOfMonth.AddDays(i));
 			}
 			return dates;
 		}
---
+++ Footballista.Wasm/Shared/Extensions/DateExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Itenso.TimePeriod;
+
+namespace Footballista.Wasm.Shared.Extensions
+{
+	public static class DateExtensions
+	{
+		public static Date AddMonths(this Date date, int monthsToAdd)
+			=> new Date(date.DateTime.AddMonths(monthsToAdd));
+		public static Date AddDays(this Date date, int daysToAdd)
+			=> new Date(date.DateTime.AddDays(daysToAdd));
+	}
+}
