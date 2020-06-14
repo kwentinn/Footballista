df2d74b3906701849ea713f86b98f7136d6bad33
kwentinn <kwentinn@gmail.com>
2020-06-14 18:30:40 +0200

    update career date service

--- Footballista.Wasm/Client/ClientServices/CareerDateService.cs
+++ Footballista.Wasm/Client/ClientServices/CareerDateService.cs
@@ -10,15 +10,14 @@ namespace Footballista.Wasm.Client.ClientServices
 	public class CareerDateService
 	{
 		public CultureInfo CultureInfo => CultureInfo.DefaultThreadCurrentCulture;
+		public DateTimeFormatInfo DateTimeInfo => CultureInfo.DateTimeFormat;
 
 		public Calendar Calendar => CultureInfo.Calendar;
 
-		public DayOfWeek FirstDayOfWeek => CultureInfo.InvariantCulture.DateTimeFormat.FirstDayOfWeek;
+		public DayOfWeek FirstDayOfWeek => CultureInfo.DateTimeFormat.FirstDayOfWeek;
+		public DayOfWeek LastDayOfWeek => DaysOfWeekEnumerable.Last();
 
-		//public string GetDisplayDayOfWeek(DayOfWeek dow)
-		//{
-
-		//}
+		public string GetDisplayDayOfWeek(DayOfWeek dow) => DateTimeInfo.GetDayName(dow);
 		public string GetDisplayDay(Date date) => date.DateTime.ToString("dddd", CultureInfo);
 		public string GetDisplayMonth(Date date) => date.DateTime.ToString("MMMM", CultureInfo);
 		public string GetDisplayYear(Date date) => date.DateTime.ToString("yyyy", CultureInfo);
@@ -30,7 +29,7 @@ namespace Footballista.Wasm.Client.ClientServices
 		public List<Date> GetDates(Date date)
 		{
 			Date firstDayOfMonth = new Date(date.Year, date.Month, 1);
-			
+
 			Console.WriteLine(firstDayOfMonth);
 
 			List<Date> dates = new List<Date>();
@@ -46,10 +45,65 @@ namespace Footballista.Wasm.Client.ClientServices
 				.OfType<DayOfWeek>()
 				.OrderBy(day => day < FirstDayOfWeek);
 
-		public CareerDateService()
+		public List<Date> GetDaysBeforeFirstDayOfWeek(Date date)
 		{
-			Console.WriteLine("culture: ");
-			Console.WriteLine(CultureInfo);
+			List<Date> datesBeforeFirstDay = new List<Date>();
+
+			//starts at first day of the month
+			var tmpDate = date.FirstDayOfMonth();
+
+			while (tmpDate.DateTime.DayOfWeek != FirstDayOfWeek)
+			{
+				tmpDate = tmpDate.AddDays(-1);
+				datesBeforeFirstDay.Insert(0, tmpDate);
+			}
+
+			return datesBeforeFirstDay;
+		}
+		public List<Date> GetDaysAfterLastDayOfWeek(Date date)
+		{
+			List<Date> datesAfterLastDay = new List<Date>();
+
+			//starts at first day of the month
+			var tmpDate = date.LastDayOfMonth();
+
+			while (tmpDate.DateTime.DayOfWeek != LastDayOfWeek)
+			{
+				tmpDate = tmpDate.AddDays(1);
+				datesAfterLastDay.Add(tmpDate);
+			}
+
+			return datesAfterLastDay;
+		}
+		public List<Date> GetDatesForMonthlyCalendar(
+			Date date,
+			bool includeDatesBeforeFirstDayOfMonth = true,
+			bool includeDatesAfterLastDayOfMonth = true
+		)
+		{
+			List<Date> allDates = new List<Date>();
+
+			if (includeDatesBeforeFirstDayOfMonth)
+			{
+				var datesBeforeFirstDay = GetDaysBeforeFirstDayOfWeek(date);
+				if (datesBeforeFirstDay.Any())
+				{
+					allDates.AddRange(datesBeforeFirstDay);
+				}
+			}
+
+			allDates.AddRange(GetDates(date));
+
+			if (includeDatesAfterLastDayOfMonth)
+			{
+				var datesAfterLastDay = GetDaysAfterLastDayOfWeek(date);
+				if (datesAfterLastDay.Any())
+				{
+					allDates.AddRange(datesAfterLastDay);
+				}
+			}
+
+			return allDates;
 		}
 	}
 }
