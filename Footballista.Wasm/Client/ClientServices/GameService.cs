d2716a4f80527dd3de29f53fadd466cc56e73962
Quentin Villevieille <quentin.villevieille@studi.fr>
2020-06-15 09:03:37 +0200

    client game svc

---
+++ Footballista.Wasm/Client/ClientServices/GameService.cs
@@ -0,0 +1,15 @@
+﻿using Footballista.Wasm.Shared.Data.Careers;
+using Itenso.TimePeriod;
+
+namespace Footballista.Wasm.Client.ClientServices
+{
+	public class GameService
+	{
+		public Career CurrentGame { get; private set; }
+
+		public void Load()
+		{
+			CurrentGame = new Career("Dummy", new Date(2020, 6, 4));
+		}
+	}
+}
