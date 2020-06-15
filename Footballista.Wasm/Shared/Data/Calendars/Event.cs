75b33f9fcf2ef3c7a55462251c1cb8261afa4e3e
Quentin Villevieille <quentin.villevieille@studi.fr>
2020-06-15 09:04:40 +0200

    classes for calendar & game svc

---
+++ Footballista.Wasm/Shared/Data/Calendars/Event.cs
@@ -0,0 +1,19 @@
+﻿using Itenso.TimePeriod;
+
+namespace Footballista.Wasm.Shared.Data.Calendars
+{
+	public class Event
+	{
+		public int Id { get; }
+		public string Name { get; }
+		public Date Date { get; }
+		public string Description { get; }
+
+		public Event(int id, string name, Date date, string description = null)
+		{
+			Id = id;
+			Name = name;
+			Date = date;
+		}
+	}
+}
---
+++ Footballista.Wasm/Shared/Data/Careers/Career.cs
@@ -0,0 +1,19 @@
+﻿using Itenso.TimePeriod;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Footballista.Wasm.Shared.Data.Careers
+{
+	public class Career
+	{
+		public string Name { get; private set; }
+		public Date CurrentDate { get; private set; }
+
+		public Career(string name, Date currentDate)
+		{
+			Name = name;
+			CurrentDate = currentDate;
+		}
+	}
+}
