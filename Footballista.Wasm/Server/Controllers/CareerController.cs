da701f14f8923d8ec04137ec6102a6f16bd78e59
Quentin Villevieille <quentin.villevieille@studi.fr>
2020-06-15 17:59:52 +0200

    add new service
    server

---
+++ Footballista.Wasm/Server/Controllers/CareerController.cs
@@ -0,0 +1,25 @@
+﻿using Footballista.Wasm.Server.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Footballista.Wasm.Server.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class CareerController : ControllerBase
+	{
+		private readonly ICareerService _careerService;
+
+		public CareerController(ICareerService careerService)
+		{
+			_careerService = careerService;
+		}
+
+		[HttpGet]
+		[Route("all")]
+		public List<string> GetAll()
+		{
+			return _careerService.GetAll();
+		}
+	}
+}
---
+++ Footballista.Wasm/Server/Services/ICareerService.cs
@@ -0,0 +1,23 @@
+﻿using Footballista.Wasm.Shared.Data.Careers;
+using System.Collections.Generic;
+
+namespace Footballista.Wasm.Server.Services
+{
+	public interface ICareerService
+	{
+		List<string> GetAll();
+		Career GetByName(string careerName);
+	}
+	public class CareerService : ICareerService
+	{
+		public List<string> GetAll()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public Career GetByName(string careerName)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
