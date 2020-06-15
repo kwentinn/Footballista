9209ee9799eb51784eeaa2afa3bf9ca428f10e5f
Quentin Villevieille <quentin.villevieille@studi.fr>
2020-06-15 09:03:24 +0200

    client calendar svc

---
+++ Footballista.Wasm/Client/ClientServices/CalendarService.cs
@@ -0,0 +1,19 @@
+﻿using Footballista.Wasm.Shared.Data.Calendars;
+using Itenso.TimePeriod;
+using System.Collections.Generic;
+
+namespace Footballista.Wasm.Client.ClientServices
+{
+	public class CalendarService
+	{
+		public List<Event> GetEventsForDate(Date date)
+		{
+			return new List<Event>
+			{
+				new Event(1, "[L1, J34] MHSC - PSG", new Date(2020, 6, 13)),
+				new Event(1, "[L1, J35] OL - MHSC", new Date(2020, 6, 20)),
+				new Event(1, "[L1, J35] OL - MHSC", new Date(2020, 6, 28)),
+			};
+		}
+	}
+}
