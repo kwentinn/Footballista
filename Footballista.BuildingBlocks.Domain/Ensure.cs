7ddf364466cce709a9d205f2355f693a203d25d1
kwentinn <kwentinn@gmail.com>
2020-07-26 10:49:27 +0200

    utility

---
+++ Footballista.BuildingBlocks.Domain/Ensure.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Footballista.BuildingBlocks.Domain
+{
+	public static class Ensure
+	{
+		public static void IsNotNull<T>(T parameter, string parameterName) where T : class
+		{
+			if (parameter == null)
+			{
+				throw new ArgumentNullException(parameterName);
+			}
+		}
+		public static void IsNotNullOrEmpty(string stringParameter, string parameterName)
+		{
+			if (string.IsNullOrEmpty(stringParameter))
+			{
+				throw new ArgumentNullException(parameterName);
+			}
+		}
+	}
+}
